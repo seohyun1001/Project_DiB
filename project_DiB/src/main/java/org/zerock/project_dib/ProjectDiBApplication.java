19773dd0d0a5b5adfb23c2c262c061c5d0e4971c
seohyun1001 <sh54991210@gmail.com>
2024-06-18 15:31:03 +0900

    240618 최초 등록

---
+++ project_DiB/src/main/java/org/zerock/project_dib/ProjectDiBApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.project_dib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDiBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectDiBApplication.class, args);
+    }
+
+}
---
+++ project_DiB/src/test/java/org/zerock/project_dib/ProjectDiBApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.project_dib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDiBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
